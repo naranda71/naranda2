8f2bbd3fa1db1118cda0bb2b13655977a3ad100c
naranda71 <naranda@tec.mx>
2024-06-12 19:28:10 +0000

    agregue accion para azure

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,19 @@
-// Placeholder to host a component
+// components/Hours.js
+import React from 'react';
+ 
+const Hours = () => {
+  const hours = Array.from({ length: 24 }, (_, i) => i);
+ 
+  return (
+<div>
+<h2>Horas del día</h2>
+<ul>
+        {hours.map((hour) => (
+<li key={hour}>{hour}:00</li>
+        ))}
+</ul>
+</div>
+  );
+};
+ 
+export default Hours;// Placeholder to host a component
